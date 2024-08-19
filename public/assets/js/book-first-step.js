502d76fffb7ff8b69a8c18c1ba6c9376452ffa9a
Crownbackend <crownbackend@gmail.com>
2024-08-20 01:43:50 +0200

    edit

--- public/assets/js/book-first-step.js
+++ public/assets/js/book-first-step.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    if ($('.select').length > 0) {
-        $('.select').on('select2:select', function (e) {
-            console.log(e.params.data.element)
+    var $select = $('.select')
+    var $price = $('#price')
+    if ($select.length > 0) {
+        $price.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
+
+        $select.on('select2:select', function (e) {
+            console.log()
+            console.log()
+            if($(e.params.data.element).data('price-hour')) {
+                $price.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
+            } else if($(e.params.data.element).data('price-day')) {
+                $price.html("€" + $select.select2().find(":selected").data("price-day") + '/j')
+            }
         });
     }
 })
