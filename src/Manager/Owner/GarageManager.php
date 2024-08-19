132b6fdd5918f2b27e77f2000f9c3b0bbf9f0653
Crownbackend <crownbackend@gmail.com>
2024-08-19 03:00:37 +0200

    edit

--- src/Manager/Owner/GarageManager.php
+++ src/Manager/Owner/GarageManager.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use App\Repository\CityRepository;
 use App\Repository\GarageRepository;
 use App\Service\FileUploader;
+use App\Service\S3Storage;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query;
 use Symfony\Component\Form\Form;
@@ -16,7 +17,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 readonly class GarageManager
 {
     public function __construct(private readonly GarageRepository $garageRepository, private readonly CityRepository $cityRepository,
-                                private readonly FileUploader $uploader, private readonly EntityManagerInterface $entityManager)
+                                private readonly FileUploader $uploader, private readonly EntityManagerInterface $entityManager, private S3Storage $s3Storage)
     {
     }
 
@@ -90,6 +91,8 @@ readonly class GarageManager
 
         $imagesFiles = $form->get("images")->getData();
         if ($imagesFiles) {
+            $result = $this->s3Storage->upload($imagesFiles[0]);
+            dd($imagesFiles[0]);
             foreach ($imagesFiles as $datum) {
                 /** @var UploadedFile $datum */
                 $imageFileName = $this->uploader->upload($datum);
