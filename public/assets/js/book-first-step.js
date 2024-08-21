d1ef38d0f409e5340d19f32d04c55f84fe883f1b
Crownbackend <crownbackend@gmail.com>
2024-08-21 02:09:35 +0200

    update

--- public/assets/js/book-first-step.js
+++ public/assets/js/book-first-step.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
 
     var $select = $('.select')
     var $price = $('#price')
-    var $priceResult = $('#price_result')
+    var $priceResult = $('.price_result')
+    var $timers = $('.timepicker')
+    var $hourContent = $('#hour_content')
+    var $endDay = $('#end_day')
+
     if ($select.length > 0) {
         $price.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
         $priceResult.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
@@ -11,9 +15,14 @@ $(document).ready(function () {
             if($(e.params.data.element).data('price-hour')) {
                 $price.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
                 $priceResult.html("€" + $select.select2().find(":selected").data("price-hour") + '/h')
+                $endDay.hide()
+                $hourContent.show()
             } else if($(e.params.data.element).data('price-day')) {
                 $price.html("€" + $select.select2().find(":selected").data("price-day") + '/j')
                 $priceResult.html("€" + $select.select2().find(":selected").data("price-day") + '/j')
+                $endDay.show()
+                $hourContent.hide()
+                $timers.val('')
             }
         });
     }
@@ -30,9 +39,24 @@ $(document).ready(function () {
         });
     }
 
+    if ($('#datetimepickershow_start').length > 0) {
+        $('#datetimepickershow_start').datetimepicker({
+            locale: 'fr',
+            inline: true,
+            sideBySide: true,
+            minDate:new Date(),
+            format: 'DD-MM-YYYY',
+            icons: {
+                up: "fas fa-angle-up",
+                down: "fas fa-angle-down",
+                next: 'fas fa-angle-right',
+                previous: 'fas fa-angle-left'
+            }
+        });
+    }
 
-    if ($('#datetimepickershow').length > 0) {
-        $('#datetimepickershow').datetimepicker({
+    if ($('#datetimepickershow_end').length > 0) {
+        $('#datetimepickershow_end').datetimepicker({
             locale: 'fr',
             inline: true,
             sideBySide: true,
@@ -43,8 +67,53 @@ $(document).ready(function () {
                 next: 'fas fa-angle-right',
                 previous: 'fas fa-angle-left'
             }
-
         });
+        $('#datetimepickershow_end').data("DateTimePicker").clear();
     }
+
+    $('#send').click(function (e) {
+        let startDate = null;
+        let endDate = null;
+        var isValid = true;
+        var previousTime = null;
+        let times = []
+
+        if ($('#datetimepickershow_start').length > 0) {
+            startDate = $('#datetimepickershow_start').data('DateTimePicker').date();
+        }
+        if ($('#datetimepickershow_end').length > 0) {
+            endDate = $('#datetimepickershow_end').data('DateTimePicker').date();
+        }
+
+        if($select.val() === 'h') {
+            $timers.each(function (index) {
+                var currentTime = $(this).val(); // Récupérer la valeur de l'heure sélectionnée
+                var currentTimeMoment = moment(currentTime, 'HH:mm'); // Convertir en objet moment
+
+                // Comparer l'heure actuelle avec l'heure précédente
+                if (previousTime && currentTimeMoment.isBefore(previousTime)) {
+                    isValid = false;
+                    alert("L'heure de fin ne peut pas être avant l'heure de début.");
+                    return false; // Sortir de la boucle
+                }
+
+                // Mettre à jour l'heure précédente pour la prochaine comparaison
+                previousTime = currentTimeMoment;
+                times.push(currentTime)
+            });
+            console.log(times)
+        } else if ($select.val() === 'j') {
+            if(startDate && endDate) {
+                console.log('dates')
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Il faut une date de début et une date de fin !",
+                });
+            }
+        }
+
+
+    })
 
 })
